60408e907b9b8da96a757d111c18e3b0db4cf067
Midi <1076735024@qq.com>
2022-03-10 21:48:11 +0800

    init

--- Tools/Burpsuite/app.py
+++ Tools/Burpsuite/app.py
@@ -1,3 +1,4 @@
+import requests
 
 
 class BpParser(object):
@@ -11,7 +12,12 @@ class BpParser(object):
         else:
             self.protocol_header = "https://"
         self.host = self.msg[1].split(" ")[1].strip()
-        self.useragent = self.msg[2].replace("User-Agent: ", "").strip()
+
+        self.headers = dict()
+        headers_key = ["User-Agent", "Accept", "Accept-Language", "Accept-Encoding", "DNT", "X-Forwarded-For", "Connection", "Upgrade-Insecure-Requests"]
+        for m in self.msg:
+            if m.split(" ")[0][:-1] in headers_key:
+                self.headers[m.split(" ")[0][:-1]] = " ".join(m.split(" ")).replace(m.split(" ")[0], "").strip()
 
     def to_py(self):
         __python_get_code = """
@@ -24,16 +30,35 @@ class BpParser(object):
         if self.method == "GET":
             return __python_get_code.format(self.useragent, self.protocol_header + self.host + self.path)
 
+
+    '''
+        模块加载时: config = 'Modules'
+    '''
+    def start(self, method='single', config=None):
+        url = self.host
+        if method == 'single':
+            if config != 'Modules':
+                print("[*] Do you need to change the URL?(y/N)")
+                if input().upper() or 'N' == 'y':
+                    url = input("url: ")
+
+            resp = requests.get(url="{}".format(self.protocol_header + url + self.path), headers=self.headers)
+
+            print(self.headers)
+
+        else:
+            ''' Multiple url'''
+            pass
+
+
+
     def run(self):
-        print(self.msg)
-        print(self.method)
-        print(self.path)
-        print(self.host)
-        print(self.useragent)
+        pass
 
 
 if __name__ == '__main__':
     with open('demo.txt', 'r') as f:
         bp = BpParser(f.readlines())
         # bp.run()
-        print(bp.to_py())
+        # print(bp.to_py())
+        bp.start()
