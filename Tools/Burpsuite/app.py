b59fce80b28d4629024750075872da3eaf1457be
Midi <1076735024@qq.com>
2022-03-25 21:22:13 +0800

    update file upload under burp

--- Tools/Burpsuite/app.py
+++ Tools/Burpsuite/app.py
@@ -1,12 +1,15 @@
 import requests
 import sys
+import re
 import urllib.parse
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 
 class BpParser(object):
     def __init__(self, msg):
         self.msg = msg
         self.method = self.msg[0].split(" ")[0]
+        self.file_upload = False
         self.path = self.msg[0].split(" ")[1]
         self.protocol = self.msg[0].split(" ")[2].strip()
         if self.protocol == "HTTP/1.1":
@@ -30,13 +33,55 @@ class BpParser(object):
             for i in range(len(msg)):
                 if self.msg[i].strip() == "":
                     self.location = i+1
-            # single row situation
+                    break
+
             # print(self.msg[self.location])
-            if self.msg[self.location].split("&"):
-                for echo in self.msg[self.location].split("&"):
-                    self.data[urllib.parse.unquote(echo.split("=")[0].replace("+", " "))] = urllib.parse.unquote(echo.split("=")[1].replace("+", " "))
+            if self.msg[self.location].startswith("---"):
+                # form-data
+                # Same as above
+                self.file_upload_flag = True
+                self.boundary = self.msg[self.location].strip()
+                file_content_keys = ["Content-Disposition", "Content-Type"]
+                self.file_content_header = dict()
+                self.file_content = ""
+                self.file_content_start_location = 0
+                for index in range(self.location, len(msg)):
+                    # print(msg[index].strip())
+                    if msg[index].startswith("------------------------"):
+                        continue
+                    elif msg[index].strip() == "":
+                        self.file_content_start_location = index + 1
+                        break
+                    else:
+                        if msg[index].strip().split(" ")[0][:-1] in file_content_keys:
+                            self.file_content_header[msg[index].split(" ")[0][:-1]] = msg[index].strip().replace(msg[index].split(" ")[0], "").strip()
+                # print(self.file_content_header)
+                # print(self.file_content_header["Content-Disposition"])
+                pat = r'(?P<type>.*?); name="(?P<name>.*?)"; filename="(?P<filename>.*?)"'
+                file_msg = re.search(pat, self.file_content_header["Content-Disposition"])
+                if file_msg:
+                    self.file_content_type = file_msg['type']
+                    self.file_content_name = file_msg['name']
+                    self.file_content_file_name = file_msg['filename']
+                else:
+                    print("[!] The file content header may error")
+                    sys.exit()
+
+                for index in range(self.file_content_start_location, len(msg)):
+                    if msg[index].startswith("------------------------"):
+                        break
+                    else:
+                        self.file_content = self.file_content + msg[index].strip() + "\n"
+                # print(self.file_content)
+
             else:
-                self.data[urllib.parse.unquote(self.msg[self.location].split("=")[0].replace("+", " "))] = urllib.parse.unquote(self.msg[self.location].split("=")[1].replace("+", " "))
+                # single row situation
+                # just post some data
+                if self.msg[self.location].split("&"):
+                    for echo in self.msg[self.location].split("&"):
+                        self.data[urllib.parse.unquote(echo.split("=")[0].replace("+", " "))] = urllib.parse.unquote(echo.split("=")[1].replace("+", " "))
+                else:
+                    self.data[urllib.parse.unquote(self.msg[self.location].split("=")[0].replace("+", " "))] = urllib.parse.unquote(self.msg[self.location].split("=")[1].replace("+", " "))
 
     def to_py(self):
         __python_get_code = """
@@ -52,12 +97,31 @@ data = {}
 resp = requests.post(url="{}", headers=headers, data=data)
 print(resp.text)        
         """
+        __python_post_file_content_code = """
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+headers = {0}
+fileDict = dict()
+fileDict["{1}"] = ("{2}", r'''{3}''', "{4}")
+m = MultipartEncoder(fields=fileDict, boundary="-------{5}")
+headers['Content-Type'] = m.content_type
+resp = requests.post(url="{6}", headers=headers, data=m, timeout=3)
+print(resp.text)
+"""
+
         if self.method == "GET":
             return __python_get_code.format(self.headers, self.protocol_header + self.host + self.path)
 
-        if self.method == "POST":
+        if self.method == "POST" and self.file_upload_flag == False:
             return __python_post_code.format(self.headers, self.data, self.protocol_header + self.host + self.path)
 
+        if self.method == "POST" and self.file_upload_flag:
+            return __python_post_file_content_code.format(self.headers,
+                                                          self.file_content_name, self.file_content_file_name,
+                                                          self.file_content.strip(), self.file_content_header['Content-Type'],
+                                                          self.boundary.replace("-", ""),
+                                                          self.protocol_header + self.host + self.path
+                                                          )
     '''
         模块加载时: config = 'Modules'
     '''
@@ -74,11 +138,20 @@ print(resp.text)
                     resp = requests.get(url="{}".format(self.protocol_header + url + self.path), headers=self.headers, timeout=3)
                     print(resp.text)
                     return resp.text
-                elif self.method == "POST":
+                elif self.method == "POST" and self.file_upload_flag == False:
                     resp = requests.post(url="{}".format(self.protocol_header + url + self.path), headers=self.headers, data=self.data, timeout=3)
                     print(resp.status_code)
                     print(resp.text)
                     return resp.text
+                elif self.method == "POST" and self.file_upload_flag:
+                    fileDict = {"{}".format(self.file_content_name): (
+                        "{}".format(self.file_content_file_name), r'''{}'''.format(self.file_content),
+                        "{}".format(self.file_content_header['Content-Type']))}
+                    m = MultipartEncoder(fields=fileDict, boundary="-------{}".format(self.boundary.replace("-", "")))
+                    self.headers['Content-Type'] = m.content_type
+                    resp = requests.post(url="{}".format(self.protocol_header + url + self.path), headers=self.headers, data=m, timeout=3)
+                    print(resp.text)
+
             except Exception as e:
                 print(e)
                 return 0
